dd2586c69419353a0297648c8605a337a6ee0f34
yitznewton <yitznewton@hotmail.com>
2019-06-17 20:27:48 -0400

    add acceptance test

--- code-challenge/spec/conway.spec.js
+++ code-challenge/spec/conway.spec.js
@@ -26,4 +26,32 @@ describe("Conway's Game of Life", () => {
       expect(conway(conway(initialState))).toEqual(initialState);
     });
   });
+
+  describe('Toad, period 2', () => {
+    const initialState = [
+      [0,0,0,0,0,0],
+      [0,0,0,0,0,0],
+      [0,0,1,1,1,0],
+      [0,1,1,1,0,0],
+      [0,0,0,0,0,0],
+      [0,0,0,0,0,0]
+    ];
+
+    const alternateState = [
+      [0,0,0,0,0,0],
+      [0,0,0,1,0,0],
+      [0,1,0,0,1,0],
+      [0,1,0,0,1,0],
+      [0,0,1,0,0,0],
+      [0,0,0,0,0,0]
+    ];
+
+    it('generates expected second state', () => {
+      expect(conway(initialState)).toEqual(alternateState);
+    });
+
+    it('oscillates back to initial state', () => {
+      expect(conway(conway(initialState))).toEqual(initialState);
+    });
+  });
 });
